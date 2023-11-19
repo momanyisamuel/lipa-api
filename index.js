f50b5a6e255479dcbb3747803a634dce66c57f1d
momanyisamuel <momanyisamuel48@gmail.com>
2023-11-19 23:39:30 +0300

    db connection test

--- index.js
+++ index.js
@@ -4,8 +4,21 @@ let ProductModel = require('./models/product');
 const app = express()
 const PORT = process.env.PORT || 3000
 app.get('/', async (req, res) => {
-    console.log("Just got a request!")
     const product = await ProductModel.find()
+    if(product) {
+        res.send({product})
+    }
+})
+app.post('/products', async (req, res) => {
+    const product = new ProductModel({
+        name: req.body.name,
+        price: req.body.price,
+        category: req.body.category,
+        quantity: req.body.quantity,
+        stock: req.body.stock,
+        img: req.body.img,
+    })
+    await product.save()
     if(product) {
         res.send({product})
     }
